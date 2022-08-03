937a1fc173b1149839d203eb78456d0f979e5053
Clifford Goh <cliffordgoh25@gmail.com>
2022-08-03 15:27:13 +0800

    added functionality to display header info

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+	
+var requestIp = require('request-ip');
+var os = require('os');
+
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -22,6 +27,15 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', function(req, res)  {
+
+  res.json({
+    ipaddress: req.ip,
+    language: req.header("accept-language"),
+    software: req.header("user-agent")
+  })
 });
 
 // listen for requests :)
